afd121053dcba4cd7085cdd3e2d7e628db486ba1
thesandman <sandman@thesandmans-MacBook-Air.local>
2025-08-24 16:42:58 -0400

    Remove deprecated appDir experimental option

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   // Only build API routes
-  experimental: {
-    appDir: true,
-  },
   // Disable static generation for API routes
-  output: 'standalone',
+  output: "standalone",
 }
 
 module.exports = nextConfig
